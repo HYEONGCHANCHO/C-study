dfff8f9d204e3c66fb5ecf7b8ab1952c40f6c765
CHO HYEONGCHAN <118033064+HYEONGCHANCHO@users.noreply.github.com>
2023-02-04 15:39:45 +0900

    c-study

---
+++ 별찍기.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    // for (i = 0; i < 5; i++)
+    // {
+    //     for (j = 1; j <= i+1; j++)
+    //     {
+    //         printf("*");
+    //     }
+    //     printf("\n");
+    // }
+
+    for(i=0;)
+
+    return 0;
+}
